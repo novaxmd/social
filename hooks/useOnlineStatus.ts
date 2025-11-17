2a5521436442c775040924414f26327bf1c50697
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-11-18 02:32:25 +0300

    Create useOnlineStatus.ts

---
+++ hooks/useOnlineStatus.ts
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+// Hook for online/offline status
+export default function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    setIsOnline(navigator.onLine);
+
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline;
+            }
+            
